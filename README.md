dc67545ccc187f3c56f2548fbc0c1331b0ca62de
mtkgeek <okolomoses59@gmail.com>
2019-03-19 18:16:00 +0100

    upload files

---
+++ README.md
@@ -0,0 +1,2 @@
+# educa
+An online learning platform much like udacity, cousera, etc. This web app was entirely written in django, apis were made using django's rest framework. Run "pip install requirements.txt" to install dependencies if you want to test the app locally.
---
+++ educa/settings/pro.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('Antonio M', 'email@mydomain.com'),
+)
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa',
+        'USER': 'xxxxxxx',
+        'PASSWORD': 'xxxxxx',
+    }
+}
